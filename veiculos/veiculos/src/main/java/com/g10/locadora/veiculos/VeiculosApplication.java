1831073bd217f2b70f9fe12148f291a7311c03c7
Gustavo-cabral-01 <gustavocabral72outlook.com@gmail.com>
2026-06-08 11:07:18 -0300

    Configuracao Eureka

--- veiculos/veiculos/src/main/java/com/g10/locadora/veiculos/VeiculosApplication.java
+++ veiculos/veiculos/src/main/java/com/g10/locadora/veiculos/VeiculosApplication.java
@@ -2,12 +2,14 @@ package com.g10.locadora.veiculos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VeiculosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VeiculosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VeiculosApplication.class, args);
+    }
 
 }
